446245c9598c59c143647bb9579c5229bdb91c32
Nicole J. Nobles <shecodez@gmail.com>
2023-02-09 16:26:13 -0500

    🐛 fix: robots.txt

--- robots.config.ts
+++ robots.config.ts
@@ -1,8 +1,10 @@
 export default [
   { UserAgent: '*' },
-  { Disallow: '/' },
+  { Allow: '/' },
   { BlankLine: true },
+
   { Comment: 'Algolia-Crawler-Verif: 79D40442AEBAFA80' },
+  { BlankLine: true },
 
   { Sitemap: (req: { headers: { host: any } }) => `https://${req.headers.host}/sitemap.xml` },
 ]
